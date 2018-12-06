7c140383650ddd3147d8eb3f6535f3fcd6c40fce
carmona44 <daniel_95ca@hotmail.com>
2018-12-06 19:09:44 +0100

    Updates

---
+++ precache-manifest.a27cb4412e963e8ac90b3181424707a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/random-quote-machine/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b8507572375d5b5ea7f9",
+    "url": "/random-quote-machine/static/js/runtime~main.b8507572.js"
+  },
+  {
+    "revision": "262643c6742734700560",
+    "url": "/random-quote-machine/static/js/main.262643c6.chunk.js"
+  },
+  {
+    "revision": "ae8ade5eb588ee75c915",
+    "url": "/random-quote-machine/static/js/1.ae8ade5e.chunk.js"
+  },
+  {
+    "revision": "262643c6742734700560",
+    "url": "/random-quote-machine/static/css/main.91355d8a.chunk.css"
+  },
+  {
+    "revision": "9d67f3aab3c2d6c0d7975d468dc474ea",
+    "url": "/random-quote-machine/index.html"
+  }
+];
